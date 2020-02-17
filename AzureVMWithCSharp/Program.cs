3d03cee71cccb4e5b6eb53b8cf7d3824d08110c3
Alexis Mateo <alexismateod@outlook.com>
2020-02-17 09:18:25 -0400

    template arm

---
+++ AzureVMWithCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using dotenv.net;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace AzureVMWithCSharp
+{
+    class Program
+    {
+        static string CLIENT_ID=Environment.GetEnvironmentVariable("CLIENT_ID");
+        static string CLIENT_SECRET=Environment.GetEnvironmentVariable("CLIENT_SECRET");
+        static string TENANT_ID=Environment.GetEnvironmentVariable("TENANT_ID");
+        static IAzure azure = null;
+        static void Main(string[] args)
+        {
+            DotEnv.Config();
+
+
+            var credentials = SdkContext.AzureCredentialsFactory
+            .FromServicePrincipal(clientId:CLIENT_ID, clientSecret:CLIENT_SECRET,tenantId:TENANT_ID,AzureEnvironment.AzureGlobalCloud);
+
+            azure = Azure
+                .Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+            
+            createResourceGroup("rg-from-csharp");
+        }
+
+        static void createResourceGroup(string resourceGroupName)
+        {
+            azure.ResourceGroups.Define(resourceGroupName)
+            .WithRegion(Region.USEast2)
+            .Create();
+        }
+    }
+}
